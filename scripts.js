bd8415bdcb6fd2eedba9eba08e0b2351ea2f865e
Andrew Russo <russspruce@gmail.com>
2016-04-12 09:16:31 -0700

    Made changes to the script to show demonstration of business logic and user interface.

--- scripts.js
+++ scripts.js
@@ -1,31 +1,30 @@
+// Business Logic
+var pingPongArray = [];
 
+var pingPong = function(numbers) {
+  for (var i = 1; i <= numbers; i++) {
+    if (i % 15 === 0) {
+      pingPongArray.push ("PING PONG!");
+    } else if (i % 5 === 0) {
+      pingPongArray.push ("PONG!");
+    } else if (i % 3 === 0) {
+      pingPongArray.push ("PING!");
+    } else {
+      pingPongArray.push (i);
+    };
+  };
+};
+
+// User Interface Logic
 $(document).ready(function() {
-  $("#pingPongForm").submit(function(event) {
-   event.preventDefault();
+  $("form#pingPongForm").submit(function(event) {
+    event.preventDefault();
 
-   var numberInput = parseInt($("#numberInput").val());
-   var start = 1;
-debugger;
-   var list = [];
-   var finalArray = [];
-    for (var index = start; index <= numberInput; index++) {
+    var numberRange = $("input#numberInput").val();
+    var result = pingPong(numberRange);
 
-      list.push(index);
-
-
-   }
-   for (var x=1; x <= list.length; x++){
-       if( x % 3 && x % 5 ) {
-           finalArray = list.replace(x, "pingpong");
-       } else {
-           if( x % 3 == 0 ) {
-               finalArray = list.replace(x, "ping");
-           }
-           if( x % 5 == 0 ) {
-               finalArray = list.replace(x, "pong");
-           }
-       }
-       $("#result").text(finalArray);
-   }
- });
-});
+    for (var i = 0; i < numberRange; i++) {
+      $("ul#result").append("<li>" + pingPongArray[i] + "</li>")
+    }; 
+  });
+}); 
